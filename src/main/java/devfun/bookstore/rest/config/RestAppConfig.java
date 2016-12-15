e1c90eaf5b64ddb8b98c575f8edcf55c601a3ba3
yhkim <xfile0304@gmail.com>
2016-12-15 17:08:21 +0900

    오류 없도록 수정

--- src/main/java/devfun/bookstore/rest/config/RestAppConfig.java
+++ src/main/java/devfun/bookstore/rest/config/RestAppConfig.java
@@ -17,7 +17,6 @@ import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
--- src/main/java/devfun/bookstore/rest/exception/ResourceNotFoundException.java
+++ src/main/java/devfun/bookstore/rest/exception/ResourceNotFoundException.java
@@ -2,4 +2,9 @@ package devfun.bookstore.rest.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9087699536649438096L;
+
 }
